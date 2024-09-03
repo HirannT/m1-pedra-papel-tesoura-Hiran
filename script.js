ead28f7a6aa291c2fe5b90d2e8bd46644947cb27
Hiran Theo <hirantheodozio@gmail.com>
2024-09-03 20:57:08 -0300

    function playRockPaperScissor

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+
+    let result = '';
+
+    if ((player1 =="Tesoura" && player2 =="Papel") || (player1 =="Pedra" && player2 =="Tesoura") || (player1 == "Papel" &&  player2 == "Pedra")) {
+        result = "Jogador 1 venceu!";
+    } else if ((player2 =="Tesoura" && player1 =="Papel") || (player2 =="Pedra" && player1 =="Tesoura") || (player2 == "Papel" &&  player1 == "Pedra")) {
+        result = "Jogador 2 venceu!";
+    } else if ((player1 =="Tesoura" && player2 =="Tesoura") || (player1 =="Pedra" && player2 =="Pedra") || (player1 == "Papel" &&  player2 == "Papel")) {
+        result = "Empate!";
+    }
+   
+    return result;
+}
